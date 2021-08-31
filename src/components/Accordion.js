c12dcd1a6dd12ad11c86d32e54ab638c5a5f7e00
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2021-08-31 15:26:52 -0400

    format CSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,6 +5,7 @@ import AccordionItem from "./AccordionItem";
 const Accordion = () => {
   const [clicked, setClicked] = useState("0");
 
+  // handler function: [0, 1, 2] index for the three FAQs
   const handleToggle = (index) => {
     if (clicked === index) {
       return setClicked("0")
